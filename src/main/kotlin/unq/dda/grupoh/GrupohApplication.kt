5451ef2259583cde994965fbaefed84413500aa0
FerRomMu <fer.rom.mu@gmail.com>
2025-04-19 15:21:31 -0300

    ft: creacion de aplicacion spring base.

---
+++ src/main/kotlin/unq/dda/grupoh/GrupohApplication.kt
@@ -0,0 +1,11 @@
+package unq.dda.grupoh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrupohApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrupohApplication>(*args)
+}
---
+++ src/test/kotlin/unq/dda/grupoh/GrupohApplicationTests.kt
@@ -0,0 +1,13 @@
+package unq.dda.grupoh
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrupohApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
