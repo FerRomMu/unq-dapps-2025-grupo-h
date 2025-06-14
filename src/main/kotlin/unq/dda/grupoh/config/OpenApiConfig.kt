b5ed37e6ae48d4fd08db2fad54ef3994d77a582b
Fernando Mario Romero Munoz <ferromer1@publicisgroupe.net>
2025-06-14 20:53:35 -0300

    fix: Ahora el swagger me permite porner bearer tokens.

---
+++ src/main/kotlin/unq/dda/grupoh/config/OpenApiConfig.kt
@@ -0,0 +1,29 @@
+package unq.dda.grupoh.config
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val securitySchemeName = "bearerAuth"
+
+        return OpenAPI()
+            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .components(
+                Components().addSecuritySchemes(
+                    securitySchemeName,
+                    SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+    }
+}
